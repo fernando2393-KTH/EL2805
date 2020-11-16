fbc5df5fe640d36694cbcfc29af78c788515c379
fernando2393 <garciasanzfernando2393@gmail.com>
2020-11-16 17:11:38 +0100

    Problem 1 finished. Convergence error solved and results stored.

--- lab1/problem_1.py
+++ lab1/problem_1.py
@@ -3,7 +3,8 @@ import matplotlib.pyplot as plt
 import itertools
 from tqdm import tqdm
 
-PLOTTING = False
+FINITE = False  # Set this to true in order to play a single game
+PLOTTING = False  # Set this to true in order to plot a single game
 
 GRID = np.array([
     [1, 1, -1, 1, 1, 1, 1, 1],
@@ -21,7 +22,6 @@ DOWN = lambda pos: (pos[0] - 1, pos[1])
 LEFT = lambda pos: (pos[0], pos[1] - 1)
 RIGHT = lambda pos: (pos[0], pos[1] + 1)
 STILL = lambda pos: pos
-FINITE = False
 
 
 def compute_reward(state):
@@ -32,6 +32,7 @@ def compute_reward(state):
     else:
         return 0
 
+
 def get_possible_states():
     rows, columns = np.where(GRID == 1)
     possible_moves = [(x, y) for x, y in zip(rows, columns)]
@@ -41,6 +42,7 @@ def get_possible_states():
         states[state] = idx
     
     return possible_states, states
+
 
 def bellman(max_time):
     # Generate states and possible states
@@ -130,6 +132,7 @@ def plot_grid(player_pos, minotaur_pos):
     plt.pause(0.35)
     plt.close()
 
+
 def value_iteration(possible_states, states, gamma, tolerance):
     # Initial best value
     best_V = np.ones((len(possible_states), 1))
@@ -142,11 +145,9 @@ def value_iteration(possible_states, states, gamma, tolerance):
 
     # Iterate until convergence
     n = 0
-    while np.square(np.subtract(V, best_V)).mean() >= tolerance and n < 50:
-
+    while np.linalg.norm(V - best_V) >= tolerance and n < 50:
         # Update the value function
         V = np.copy(best_V)
-
         # Compute the new BV
         for state in possible_states:
             rewards = []
@@ -155,7 +156,8 @@ def value_iteration(possible_states, states, gamma, tolerance):
             minotaur = Minotaur()
             minotaur.position = state[1]
             if player.position == (EXIT or minotaur.position):
-                V[states[state]] = compute_reward(state)
+                best_V[states[state]] = compute_reward(state)
+                best_policy[states[state]] = [player.position]
                 continue
             p_actions = player.possible_moves()
             m_actions = minotaur.possible_moves()
@@ -165,23 +167,19 @@ def value_iteration(possible_states, states, gamma, tolerance):
                 for next_state in next_possible_states:
                     reward += gamma * (1 / len(next_possible_states)) * V[states[next_state]]
                 rewards.append(reward)
-            
             # Define best reward
             u = max(rewards)
             best_V[states[state]] = u
-
             # Compute best policy
             a = rewards.index(u)
             best_policy[states[state]] = [p_actions[a]]
-        
         n += 1
     # Return the best possible moves for each state
-    print("error: ", np.square(np.subtract(V, best_V)).mean())
+    print("error: ", np.linalg.norm(V - best_V))
     return best_policy
 
 
 def main():
-
     if FINITE:
         probabilities = []
         max_time = 20
@@ -212,7 +210,6 @@ def main():
                         print("Won after " + str(i) + " actions.")
                         exit(0)
                 print("You did not reach the exit in time!")
-
         plt.plot(range(1, len(probabilities) + 1), probabilities)
         plt.title("Probability vs Maximum Time")
         plt.xticks(np.arange(1, len(probabilities) + 1))
@@ -228,50 +225,59 @@ def main():
         epsilon = 0.0001
         # Tolerance error
         tolerance = (1 - gamma) * epsilon / gamma
-        
         # Define all states
         possible_states, states = get_possible_states()
-
         # Compute Value Iteration (VI)
         best_policy = value_iteration(possible_states, states, gamma, tolerance)
-
         # Use same policy over 10,000 games
         simulations = 10 ** 4
-        life_distribution = np.random.geometric(p = 1/mean, size = simulations)
+        life_distribution = np.random.geometric(p=1/mean, size=simulations)
         games_outcome = {
-            "win" : 0,
-            "eaten" : 0,
-            "times up" : 0
+            "win": 0,
+            "eaten": 0,
+            "times up": 0,
+            "win distribution": [],
+            "loss distribution": []
         }
-    
+
         for life_time in life_distribution:
             # Initialize players
             player = Player()
             minotaur = Minotaur()
             # Define final position
             terminal_state = False
-
             for _ in range(life_time):
                 # Get best movement based on the state (player, minotaur)
                 player.position = best_policy[states[(player.position, minotaur.position)]][0]
                 if player.position != (EXIT or minotaur.position):
                     # Generate new movement if game if not over
                     minotaur.position = minotaur.generate_move()
-                elif player.position == EXIT:
-                    games_outcome["win"] += 1
-                    terminal_state = True
-                    break
                 elif player.position == minotaur.position:
                     games_outcome["eaten"] += 1
                     terminal_state = True
+                    games_outcome["loss distribution"].append(life_time)
                     break
-            
+                elif player.position == EXIT:
+                    games_outcome["win"] += 1
+                    terminal_state = True
+                    games_outcome["win distribution"].append(life_time)
+                    break
             if not terminal_state:
                 games_outcome["times up"] += 1
+                games_outcome["loss distribution"].append(life_time)
             
         print("All games ended")
         print(games_outcome)
         print("Ratio (Not Finished)/(Won) :", games_outcome["times up"]/games_outcome["win"])
+
+        plt.hist(games_outcome["win distribution"], color="b", edgecolor="black",
+                 bins=int(len(set(games_outcome["win distribution"]))/5), label="Win")
+        plt.hist(games_outcome["loss distribution"], color="r", edgecolor="black",
+                 bins=int(len(set(games_outcome["loss distribution"]))/5), label="Loss")
+        plt.title("Wins vs Losses split by maximum time")
+        plt.xlabel("Maximum time")
+        plt.ylabel("Frequency")
+        plt.show()
 
 
 if __name__ == "__main__":
