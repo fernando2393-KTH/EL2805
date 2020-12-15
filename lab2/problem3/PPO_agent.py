07cdfab3cc5ff02e24ec09fffe34eb2715916912
Gustavo Beck <teodoro.beck@gmail.com>
2020-12-15 18:53:41 +0100

    Init problem3 and plots problem 1

--- lab2/problem3/PPO_agent.py
+++ lab2/problem3/PPO_agent.py
@@ -15,6 +15,165 @@
 
 # Load packages
 import numpy as np
+import torch
+import torch.nn as nn
+
+HIDDEN_NODES = [400, 200]
+computePDF = lambda mu, sigma_square, action: pow(2 * np.pi * sigma_square, -1/2) * np.exp(- pow(action - mu, 2) / (2 * sigma_square))
+
+class CriticNetwork(nn.Module):
+    def __init__(self, input_size, output_size, hidden_units):
+        super().__init__()
+
+        # Create input layer with ReLU activation
+        self.input_layer = nn.Linear(input_size, hidden_units[0])
+        self.input_layer_activation = nn.ReLU()
+
+        # Create middle layer with ReLU activation
+        self.middle_layer = nn.Linear(hidden_units[0], hidden_units[1])
+        self.middle_layer_activation = nn.ReLU()
+
+        # Create output layer
+        self.output_layer = nn.Linear(hidden_units[1], output_size)
+
+    def forward(self, x):
+        # Function used to compute the forward pass
+
+        # Compute first layer
+        l1 = self.input_layer(x)
+        l1 = self.input_layer_activation(l1)
+
+        # Compute second layer
+        l2 = self.middle_layer(l1)
+        l2 = self.middle_layer_activation(l2)
+
+        # Compute output layer
+        out = self.output_layer(l2)
+
+        return out
+
+
+class ActorNetwork(nn.Module):
+    """ Create a feedforward neural network """
+
+    def __init__(self, input_size, output_size, hidden_units):
+        super().__init__()
+
+        # Create input layer with ReLU activation
+        self.input_layer = nn.Linear(input_size, hidden_units[0])
+        self.input_layer_activation = nn.ReLU()
+
+        # Create middle layer head 1 - mu with ReLU activation
+        self.middle_layer_head_1 = nn.Linear(hidden_units[0], hidden_units[1])
+        self.middle_layer_activation_head_1 = nn.ReLU()
+
+        # Create output layer head 1 - mu with Tanh
+        self.output_layer_head_1 = nn.Linear(hidden_units[1], output_size)
+        self.output_layer_activation_head_1 = nn.Tanh()
+
+        # Create middle layer head 2 - sigma with ReLU activation
+        self.middle_layer_head_2 = nn.Linear(hidden_units[0], hidden_units[1])
+        self.middle_layer_activation_head_2 = nn.ReLU()
+
+        # Create output layer head 2 - sigma
+        self.output_layer_head_2 = nn.Linear(hidden_units[1], output_size)
+        self.output_layer_activation_head_2 = nn.Sigmoid()
+
+    def forward(self, x):
+        # Function used to compute the forward pass
+
+        # Compute first layer
+        l1 = self.input_layer(x)
+        l1 = self.input_layer_activation(l1)
+
+        # Compute second layer
+        l2_mu = self.middle_layer_head_1(l1)
+        l2_mu = self.middle_layer_activation_head_1(l2_mu)
+
+        # Compute output layer
+        out_mu = self.output_layer_head_1(l2_mu)
+        out_mu = self.output_layer_activation_head_1(out_mu)
+
+        # Compute second layer
+        l2_sigma = self.middle_layer_head_2(l1)
+        l2_sigma = self.middle_layer_activation_head_2(l2_sigma)
+
+        # Compute output layer
+        out_sigma = self.output_layer_head_2(l2_sigma)
+        out_sigma = self.output_layer_activation_head_2(out_sigma)
+
+        return out_mu, out_sigma
+
+class AgentQ(object):
+    """ Base agent class, used as a parent class
+
+        Args:
+            n_actions (int): number of actions
+
+        Attributes:
+            n_actions (int): where we store the number of actions
+            last_action (int): last action taken by the agent
+    """
+
+    def __init__(self, n_actions: int, dim_state: int, critic_lr, actor_lr, N_episodes, discount_factor, dev, epsilon):
+        self.last_action = None
+        self.actor_network = ActorNetwork(input_size=dim_state, output_size=n_actions,
+                                          hidden_units=HIDDEN_NODES).to(dev)
+        self.critic_network = CriticNetwork(input_size=dim_state, output_size=1,
+                                            hidden_units=HIDDEN_NODES).to(dev)
+        self.critic_optimizer = torch.optim.Adam(self.critic_network.parameters(), lr=critic_lr)
+        self.actor_optimizer = torch.optim.Adam(self.actor_network.parameters(), lr=actor_lr)
+        self.discount_factor = discount_factor
+        self.dev = dev
+        self.m = n_actions
+        self.n = np.zeros(n_actions)
+        self.epsilon = epsilon
+
+    def forward_critic(self, state):
+        """ Performs a forward computation """
+        return self.critic_network(state)
+
+    def forward_actor(self, state):
+        """ Performs a forward computation """
+        return self.actor_network(state)
+
+    def backward_critic(self, values, targets):
+        """ Performs a backward pass on the network """
+        # Compute gradient and Perform backward pass (backpropagation)
+        # Training process, set gradients to 0
+        self.critic_optimizer.zero_grad()
+
+        # Compute loss function
+        loss = nn.functional.mse_loss(
+            values,
+            targets
+        )
+
+        loss.backward()
+
+        # Clip gradient norm to 1
+        nn.utils.clip_grad_norm_(self.critic_network.parameters(), max_norm=1.)
+        self.critic_optimizer.step()
+
+    def backward_actor(self, states, psi, pdfs_old, t):
+        # Compute loss
+        self.actor_optimizer.zero_grad()
+        mu_t, sigma_square_t = self.actor_network(states)  # Compute possible actions
+        mu_t = mu_t.cpu().detach().numpy()
+        sigma_square_t = sigma_square_t.cpu().detach().numpy()
+        actions = np.array([np.random.multivariate_normal(mu, sigma * np.identity(self.m)) for mu, sigma in zip(mu_t, sigma_square_t)])
+        pdf = computePDF(mu_t[:, 0], sigma_square_t[:, 0], actions[:, 0]) * computePDF(mu_t[:, 1], sigma_square_t[:, 1], actions[:, 1])
+        r_theta = pdf / pdfs_old
+        clip = torch.clip(r_theta, (1 - self.epsilon), (1 + self.epsilon))
+        loss = - torch.mean(torch.min(r_theta.reshape(-1, 1) * psi, clip.reshape(-1, 1) * psi))
+        loss.backward()
+        # Clip gradient norm to 1
+        nn.utils.clip_grad_norm_(self.actor_network.parameters(), max_norm=1.)
+
+        # Update theta
+        self.actor_optimizer.step()
+        
+
 
 class Agent(object):
     ''' Base agent class
--- lab2/problem3/PPO_problem.py
+++ lab2/problem3/PPO_problem.py
@@ -14,12 +14,58 @@
 #
 
 # Load packages
+from collections import deque
 import numpy as np
 import gym
 import torch
 import matplotlib.pyplot as plt
 from tqdm import trange
-from PPO_agent import RandomAgent
+from PPO_agent import RandomAgent, AgentQ
+from tqdm import tqdm
+
+M = 100
+L = 30000
+computePDF = lambda mu, sigma_square, action: pow(2 * np.pi * sigma_square, -1/2) * np.exp(- pow(action - mu, 2) / (2 * sigma_square))
+
+class ExperienceReplayBuffer(object):
+    """ Class used to store a buffer containing experiences of the RL agent.
+    """
+    def __init__(self, maximum_length):
+        # Create buffer of maximum length
+        self.buffer = deque(maxlen=maximum_length)
+
+    def append(self, experience):
+        # Append experience to the buffer
+        self.buffer.append(experience)
+
+    def __len__(self):
+        # overload len operator
+        return len(self.buffer)
+
+    def sample_batch(self, n):
+        """ Function used to sample experiences from the buffer.
+            returns 5 lists, each of size n. Returns a list of state, actions,
+            rewards, next states and done variables.
+        """
+        # If we try to sample more elements that what are available from the
+        # buffer we raise an error
+        if n > len(self.buffer):
+            raise IndexError('Tried to sample too many elements from the buffer!')
+
+        # Sample without replacement the indices of the experiences
+        # np.random.choice takes 3 parameters: number of elements of the buffer,
+        # number of elements to sample and replacement.
+        indices = np.arange(len(self.buffer))
+
+        # Using the indices that we just sampled build a list of chosen experiences
+        batch = [self.buffer[i] for i in indices]
+
+        # batch is a list of size n, where each element is an Experience tuple
+        # of 5 elements. To convert a list of tuples into
+        # a tuple of list we do zip(*batch). In this case this will return a
+        # tuple of 5 elements where each element is a list of n elements.
+        return zip(*batch)
+
 
 def running_average(x, N):
     ''' Function used to compute the running average
@@ -32,82 +78,132 @@ def running_average(x, N):
         y = np.zeros_like(x)
     return y
 
-# Import and initialize Mountain Car Environment
-env = gym.make('LunarLanderContinuous-v2')
-env.reset()
+def main():
+    dev = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    print("Using", dev)
 
-# Parameters
-N_episodes = 100               # Number of episodes to run for training
-discount_factor = 0.95         # Value of gamma
-n_ep_running_average = 50      # Running average of 20 episodes
-m = len(env.action_space.high) # dimensionality of the action
+    # Import and initialize Mountain Car Environment
+    env = gym.make('LunarLanderContinuous-v2')
+    env.reset()
 
-# Reward
-episode_reward_list = []  # Used to save episodes reward
-episode_number_of_steps = []
+    # Parameters
+    N_episodes = 1600               # Number of episodes to run for training
+    discount_factor = 0.99         # Value of gamma
+    n_ep_running_average = 50      # Running average of 20 episodes
+    m = len(env.action_space.high) # dimensionality of the action
+    epsilon = 0.2
+    lr_critic = pow(10, -3)
+    lr_actor = pow(10, -5)
+    dim_state = len(env.observation_space.high)  # State dimensionality
 
-# Agent initialization
-agent = RandomAgent(m)
+    # Reward
+    episode_reward_list = []  # Used to save episodes reward
+    episode_number_of_steps = []
 
-# Training process
-EPISODES = trange(N_episodes, desc='Episode: ', leave=True)
+    # Agent initialization
+    agent = AgentQ(m, dim_state, lr_critic, lr_actor, N_episodes, discount_factor, dev, epsilon)
 
-for i in EPISODES:
-    # Reset enviroment data
-    done = False
-    state = env.reset()
-    total_episode_reward = 0.
-    t = 0
-    while not done:
-        # Take a random action
-        action = agent.forward(state)
+    # Training process
+    EPISODES = trange(N_episodes, desc='Episode: ', leave=True)
 
-        # Get next state and reward.  The done variable
-        # will be True if you reached the goal position,
-        # False otherwise
-        next_state, reward, done, _ = env.step(action)
+    for i in EPISODES:
+        # Reset enviroment data
+        done = False
+        state = env.reset()
+        total_episode_reward = 0.
+        t = 0
 
-        # Update episode reward
-        total_episode_reward += reward
+        # Initialize Buffer
+        buffer = ExperienceReplayBuffer(maximum_length=L)
+    
+        while not done:
+            # Take a random action
+            state_tensor = torch.tensor(state, device=dev, dtype = torch.float32)
+            mu_t, sigma_square_t = agent.forward_actor(state_tensor)  # Compute possible actions
+            mu_t = mu_t.cpu().detach().numpy()
+            sigma_square_t = sigma_square_t.cpu().detach().numpy()
+            action = np.random.multivariate_normal(mu_t, sigma_square_t * np.identity(m))
 
-        # Update state for next iteration
-        state = next_state
-        t+= 1
+            pdf_old = computePDF(mu_t[0], sigma_square_t[0], action[0]) * computePDF(mu_t[1], sigma_square_t[1], action[1])
+            
+            # Get next state and reward. The done variable
+            # will be True if you reached the goal position,
+            # False otherwise
+            next_state, reward, done, _ = env.step(action)
+            experience = (pdf_old, state, action, reward, next_state, done)  # Create the experience
+            buffer.append(experience)  # Append the experience to the buffer
 
-    # Append episode reward
-    episode_reward_list.append(total_episode_reward)
-    episode_number_of_steps.append(t)
+            # Update episode reward
+            total_episode_reward += reward
 
-    # Close environment
-    env.close()
+            # Update state for next iteration
+            state = next_state
+            t += 1
+        
+        for n in range(M):
+            # Get params from buffer
+            pdfs_old, states, actions, rewards, next_states, _ = buffer.sample_batch(n=t)
+            pdfs_old_tensor = torch.tensor(pdfs_old, dtype=torch.float32, device=dev)
+            states_tensor = torch.tensor(states, dtype=torch.float32, device=dev)
+            actions_tensor = torch.tensor(actions, dtype=torch.float32, device=dev)
 
-    # Updates the tqdm update bar with fresh information
-    # (episode number, total reward of the last episode, total number of Steps
-    # of the last episode, average reward, average number of steps)
-    EPISODES.set_description(
-        "Episode {} - Reward/Steps: {:.1f}/{} - Avg. Reward/Steps: {:.1f}/{}".format(
-        i, total_episode_reward, t,
-        running_average(episode_reward_list, n_ep_running_average)[-1],
-        running_average(episode_number_of_steps, n_ep_running_average)[-1]))
+            # Compute targets
+            targets_list = np.zeros(t)
+            for idx in range(t):
+                for jdx in range(idx, t):
+                    targets_list[idx] += pow(discount_factor, (jdx - idx)) * rewards[jdx]
+
+            targets = torch.tensor(targets_list, device=dev, dtype=torch.float32).reshape(-1, 1)
+
+            # Compute values
+            values = agent.forward_critic(states_tensor)
+
+            # Update w (critic network)
+            agent.backward_critic(targets, values)
+
+            # Compute Psi
+            psi = targets - values
+
+            # Update theta (actor network)
+            agent.backward_actor(states_tensor, psi, pdfs_old_tensor, n)   
+
+        # Append episode reward
+        episode_reward_list.append(total_episode_reward)
+        episode_number_of_steps.append(t)
+
+        # Close environment
+        env.close()
+
+        # Updates the tqdm update bar with fresh information
+        # (episode number, total reward of the last episode, total number of Steps
+        # of the last episode, average reward, average number of steps)
+        EPISODES.set_description(
+            "Episode {} - Reward/Steps: {:.1f}/{} - Avg. Reward/Steps: {:.1f}/{}".format(
+            i, total_episode_reward, t,
+            running_average(episode_reward_list, n_ep_running_average)[-1],
+            running_average(episode_number_of_steps, n_ep_running_average)[-1]))
 
 
-# Plot Rewards and steps
-fig, ax = plt.subplots(nrows=1, ncols=2, figsize=(16, 9))
-ax[0].plot([i for i in range(1, N_episodes+1)], episode_reward_list, label='Episode reward')
-ax[0].plot([i for i in range(1, N_episodes+1)], running_average(
-    episode_reward_list, n_ep_running_average), label='Avg. episode reward')
-ax[0].set_xlabel('Episodes')
-ax[0].set_ylabel('Total reward')
-ax[0].set_title('Total Reward vs Episodes')
-ax[0].legend()
-ax[0].grid(alpha=0.3)
+    # Plot Rewards and steps
+    fig, ax = plt.subplots(nrows=1, ncols=2, figsize=(16, 9))
+    ax[0].plot([i for i in range(1, N_episodes+1)], episode_reward_list, label='Episode reward')
+    ax[0].plot([i for i in range(1, N_episodes+1)], running_average(
+        episode_reward_list, n_ep_running_average), label='Avg. episode reward')
+    ax[0].set_xlabel('Episodes')
+    ax[0].set_ylabel('Total reward')
+    ax[0].set_title('Total Reward vs Episodes')
+    ax[0].legend()
+    ax[0].grid(alpha=0.3)
 
-ax[1].plot([i for i in range(1, N_episodes+1)], episode_number_of_steps, label='Steps per episode')
-ax[1].plot([i for i in range(1, N_episodes+1)], running_average(
-    episode_number_of_steps, n_ep_running_average), label='Avg. number of steps per episode')
-ax[1].set_xlabel('Episodes')
-ax[1].set_ylabel('Total number of steps')
-ax[1].set_title('Total number of steps vs Episodes')
-ax[1].legend()
-ax[1].grid(alpha=0.3)
-plt.show()
+    ax[1].plot([i for i in range(1, N_episodes+1)], episode_number_of_steps, label='Steps per episode')
+    ax[1].plot([i for i in range(1, N_episodes+1)], running_average(
+        episode_number_of_steps, n_ep_running_average), label='Avg. number of steps per episode')
+    ax[1].set_xlabel('Episodes')
+    ax[1].set_ylabel('Total number of steps')
+    ax[1].set_title('Total number of steps vs Episodes')
+    ax[1].legend()
+    ax[1].grid(alpha=0.3)
+    plt.show()
+
+if __name__ == "__main__":
+    main()
